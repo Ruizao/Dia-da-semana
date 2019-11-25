4f098958a2977d43ac945d8e93c6083043c27bb2
rui <rui@WIN-GESFLHLR4EE>
2019-11-25 16:07:36 +0000

    Adicionar arquivos de projeto.

---
+++ Signos/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Signos
+{
+    public partial class Form1 : Form
+    {
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        diadasemana dia = new diadasemana();
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            dia.recebe = textBox1.Text;
+            label3.Text = dia.envia;
+        }
+    }
+}
---
+++ Signos/diadasemana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signos
+{
+    class diadasemana
+    {
+        private DateTime data;
+        public string recebe
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                return data.ToString("dddd");
+            }
+        }
+    }
+}
